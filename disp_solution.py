0b67f8f2509481000aadf27419fb04090d63b266
Silvia <silvia.tulli.92@gmail.com>
2021-04-06 09:09:06 +0200

    Update disp_solution.py

--- disp_solution.py
+++ disp_solution.py
@@ -1,30 +1,25 @@
-# function to write solution to .txt file in given format
-def disp_solution(comp_tasks, st_frag, frag_t, frags, deadline_t, task_cor_frag):
-    last_fr_idx = 0
+# function to write solution to standard output
+def disp_solution(comp_tasks, n_frags, n_comp_tasks, st_frag, frag_t, task_cor_frag) -> None:
     list_comp_task = []
-    for i in range(len(frag_t)):
-        last_fr_idx += frag_t[i]
-        last_fr_st = st_frag[last_fr_idx-1]
-        task_comp_t = last_fr_st + frags[last_fr_idx-1]
-        if task_comp_t <= deadline_t[i]:
+    for i in range(len(comp_tasks)):
+        if comp_tasks[i]==1:
             list_comp_task.append(i+1)
-    # print ("list of comp tasks = ", list_comp_task)
-    frag_st_times = []
-    idx_valid_st_frag = []
-    for i in range(len(task_cor_frag)):
-        if task_cor_frag[i] in list_comp_task:
-            frag_st_times.append(st_frag[i])
-            idx_valid_st_frag.append(i)
         else:
             continue
-    # print (frag_st_times)
-    # print (idx_valid_st_frag)
+    
+    frag_st_times = []
+    for i in range(n_frags):
+        if task_cor_frag[i] in list_comp_task:
+            frag_st_times.append(st_frag[i])
+        else:
+            continue
 
-    print(str(comp_tasks))
+    print(str(n_comp_tasks))
     count = 0
-    for i in range(comp_tasks):
+    for i in range(n_comp_tasks):
         print(str(list_comp_task[i]),end="")
-        for k in range(frag_t[list_comp_task[i]-1]):
+        for _ in range(frag_t[list_comp_task[i]-1]):
             print((" "+str(frag_st_times[count])),end="")
             count += 1
         print("\n",end="")
+
